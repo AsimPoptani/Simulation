39a89d9abc8a8b3b5bff7f36d5748798afec5585
Sam Hart <wsdh500@york.ac.uk>
2022-05-12 08:40:22 +0100

    Update the Boat code.

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ WINDFARM_WIDTH = 252000 #252km
 WINDFARM_HEIGHT = WINDFARM_WIDTH
 DIRECTION_OF_WINDFARM = math.radians( 45 )
 
-# Width of the turbines
+# dimensions of the turbines
 ROTOR_DIAMETER = 154 
 ROTOR_RADIUS = ROTOR_DIAMETER // 2
 TURBINE_HEIGHT = 190
@@ -25,16 +25,27 @@ MAX_POWER = 7000000
 AIR_DENSITY = 1.225 # At sea level
 
 # TODO fix these values
+# meters per tick
 DRONE_MAX_VELOCITY = 1000
-# km
+#
 DRONE_MAX_BATTERY = 100
 # km
 DRONE_MAX_COMMUNICATION_RANGE = 10000
-
+# ticks
 DRONE_MAX_DETECTION_RANGE = 10
-
+# dimensions of drone in meters
 DRONE_RADIUS = 1
+# multiplier for safe radius
 DRONE_SAFE_ZONE = 2
 
+# meters
+BOAT_RADIUS = 14
+#
+BOAT_MAX_FUEL = 1000
+# meters per tick
+BOAT_MAX_VELOCITY = 10000
+
+# ticks
 DATA_UPDATE_INTERVAL = 20
+# value by which the probabilities are divided
 FAULT_RATE_DIVISOR = 24 * 60 * 60
