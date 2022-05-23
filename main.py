531b08fab52529b5c9db0e9b55b87138eb7b0a80
Sam Hart <wsdh500@york.ac.uk>
2022-05-23 13:33:07 +0100

    Add drone status legend.

--- main.py
+++ main.py
@@ -159,40 +159,72 @@ while running:
     summary_box_surface.fill(BG_COLOUR)
     text = font.render("Ship summary", 1, FG_COLOUR)
     summary_box_surface.blit(text, (5, 0), text.get_rect())
-
     # display state
     summary_text = "State : " + boat.state.name
     text = font.render(summary_text, 1, FG_COLOUR)
     summary_box_surface.blit(text, (5, 30), text.get_rect())
-
     # Get ship battery level
     fuel_level = boat.fuel_level
-
     # Get boat sprite
     boat_sprite = BoatSprite(boat)
     battery_icon = boat_sprite.getPower()
-
     # Add text "Total power"
     text = font.render(f"Total fuel : {((fuel_level / BOAT_MAX_FUEL) * 100):.2f}%", 1, FG_COLOUR)
     summary_box_surface.blit(text, (25, 50), text.get_rect())
-
     # Add Icon next to Total power
-    summary_box_surface.blit(battery_icon, (5, 52))
-    
+    summary_box_surface.blit(battery_icon, (5, 54))
     # Number of drones out
     # Count number of drones out
     num_out = BOAT_N_DRONES - len(boat.drones)
-
     text = font.render(f"Drones out : {num_out} | Drones in : {BOAT_N_DRONES-num_out}", 1, FG_COLOUR)
     summary_box_surface.blit(text, (25, 70), text.get_rect())
-
     # Get image of drone
     drone_sprite = pygame.image.load('./sprites/subwhite.png')
     # Add Icon next to drones out
-    summary_box_surface.blit(drone_sprite, (5, 72))
-
+    summary_box_surface.blit(drone_sprite, (5, 74))
     # Blit summary box to screen
     screen.blit(summary_box_surface, (5, HEIGHT - summary_box[1]))
+
+
+    ######## DRONE LEGEND ########
+    drone_box = (300, 100)
+    drone_box_surface = pygame.Surface(summary_box)
+    drone_box_surface.fill(BG_COLOUR)
+    text = font.render("Drone states", 1, FG_COLOUR)
+    drone_box_surface.blit(text, (5, 0), text.get_rect())
+    # collect some stats
+    holding = 0
+    moving = 0
+    detecting = 0
+    count = 0
+    for drone in boat.auvs:
+        if drone.state == VehicleStates.HOLDSTATE:
+            holding += 1
+        elif drone.state == VehicleStates.MOVESTATE or drone.state == VehicleStates.RETURNSTATE:
+            moving += 1
+        elif drone.state == VehicleStates.DETECTSTATE:
+            detecting += 1
+    # holding drone
+    drone_sprite = pygame.image.load('./sprites/subwhite.png')
+    drone_box_surface.blit(drone_sprite, (5, 34))
+    moving_state_text = VehicleStates.HOLDSTATE.name + ' : ' + str(holding)
+    text = font.render(moving_state_text, 1, FG_COLOUR)
+    drone_box_surface.blit(text, (25, 30), text.get_rect())
+    # moving drone
+    drone_sprite = pygame.image.load('./sprites/subred.png')
+    drone_box_surface.blit(drone_sprite, (5, 54))
+    moving_state_text = VehicleStates.MOVESTATE.name + ' : ' + str(moving)
+    text = font.render(moving_state_text, 1, FG_COLOUR)
+    drone_box_surface.blit(text, (25, 50), text.get_rect())
+    # detect drone
+    drone_sprite = pygame.image.load('./sprites/subgreen.png')
+    drone_box_surface.blit(drone_sprite, (5, 74))
+    moving_state_text = VehicleStates.DETECTSTATE.name + ' : ' + str(detecting)
+    text = font.render(moving_state_text, 1, FG_COLOUR)
+    drone_box_surface.blit(text, (25, 70), text.get_rect())
+    # Blit drone legend to screen
+    drone_legend_x = (2 * WIDTH) // 5
+    screen.blit(drone_box_surface, (drone_legend_x, HEIGHT - summary_box[1]))
 
 
     ######## WEATHER ########
